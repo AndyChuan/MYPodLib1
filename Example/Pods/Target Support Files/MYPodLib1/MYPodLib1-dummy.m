a33097cab620d0c25c53f650fe5740a41122a7c5
chengchuancun-a1147 <chuancun.cheng-a1147@aqara.com>
2023-03-17 19:42:32 +0800

    first commit

---
+++ Example/Pods/Target Support Files/MYPodLib1/MYPodLib1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MYPodLib1 : NSObject
+@end
+@implementation PodsDummy_MYPodLib1
+@end
---
+++ Example/Pods/Target Support Files/Pods-MYPodLib1_Example/Pods-MYPodLib1_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MYPodLib1_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MYPodLib1_Example
+@end
